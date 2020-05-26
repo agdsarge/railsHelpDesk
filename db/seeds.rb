2ab96b91f30273c66473d965286a2fa3761e051b
agdsarge <agdsargeant@gmail.com>
2020-05-26 09:58:07 -0400

    drafted seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Client.destroy_all
+Comment.destroy_all
+Isp.destroy_all
+Professional.destroy_all
+Ticket.destroy_all
+
+vz = Isp.create(name: 'Verizon FiOS', email: 'support@verizon.com')
+cx = Isp.create(name: 'Cox Cable', email: 'help@cox.com')
+cc = Isp.create(name: 'Comcast', email: 'tech_desk@comcast.com')
+el = Isp.create(name: "EarthLink", email: 'geeks@earthlink.net')
+sp = Isp.create(name: "Spectrum", email: "help@spectrum.com")
+
+t.string "name"
+t.string "phone_number"
+t.string "email"
+t.string "home_address"
+t.integer "isp_id"
+
+pn = Client.create(name: 'Paul Nicholsen', phone_number: '202 555 5678', email: 'p.nicholsen@flatiron.com', home_address: '1600 Pennsylvania Ave', isp_id: vz.id)
+sn = Client.create(name: 'Shannon Nabors', phone_number: '202 555 6543', email: 's.nabors@flatiron.com', home_address: '1440 G St. NW', isp_id: cx.id)
+ep = Client.create(name: 'Enrique Pittaluga', phone_number: '202 555 0965', email: 'e.pittaluga@flatiron.com', home_address: '1438 G St. NW', isp_id: cc.id)
+as = Client.create(name: 'Abhinav Sharma', phone_number: '202 555 3965', email: 'a.sharma@flatiron.com', home_address: '1436 G St. NW', isp_id: cc.id)
+ag = Client.create(name: 'Alex Gautherin', phone_number: '202 555 0879', email: 'a.gautherin@flatiron.com', home_address: '1434 G St. NW', isp_id: vz.id)
+ds = Client.create(name: 'Davis Sargeant', phone_number: '202 555 0182', email: 'd.sargeant@flatiron.com', home_address: '1432 G St. NW', isp_id: cx.id)
+sa = Client.create(name: 'Saima Akhtar', phone_number: '202 555 5432', email: 's.akhtar@flatiron.com', home_address: '1430 G St. NW', isp_id: el.id)
+mo = Client.create(name: 'Mert Ozkaynak', phone_number: '202 555 8899', email: 'm.ozkaynak@flatiron.com', home_address: '1428 G St. NW', isp_id: sp.id)
+jb = Client.create(name: 'Josh Beglan', phone_number: '202 555 1212', email: 'j.beglan@flatiron.com', home_address: '1426 G St. NW', isp_id: vz.id)
+jl = Client.create(name: 'Jesse Langley', phone_number: '202 555 9115', email: 'j.langley@flatiron.com', home_address: '1424 G St. NW', isp_id: cc.id)
+ta = Client.create(name: 'Tomazye Anderson', phone_number: '202 555 7531', email: 't.anderson@flatiron.com', home_address: '1424 G St. NW', isp_id: cx.id)
+il = Client.create(name: 'Injae Lee', phone_number: '202 555 1238', email: 'i.lee@flatiron.com', home_address: '1422 G St. NW', isp_id: vz.id)
+
+frodo = Professional.create(username: 'f.baggins', specialty: 'routers', email: 'f.baggins@greenleaf.com')
+sam = Professional.create(username: 's.gamgee', specialty: 'surge protectors', email: 's.gamgee@greenleaf.com')
+merry = Professional.create(username: 'm.brandybuck', specialty: 'wifi', email: 'm.brandybuck@greenleaf.com')
+pippin  = Professional.create(username: 'p.took', specialty: 'routers', email: 'p.took@greenleaf.com')
+bilbo = Professional.create(username: 'b.baggins', specialty: 'surge protectors', email: 'b.baggins@greenleaf.com')
+fredegar = Professional.create(username: 'f.bulger', specialty: 'modem', email: 'f.bulger@greenleaf.com')
