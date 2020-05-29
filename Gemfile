5484e0a8c7bb1f9a2c11973f20161e8b275d31b4
agdsarge <agdsargeant@gmail.com>
2020-05-29 12:06:39 -0400

    rudimentary rspec tests

--- Gemfile
+++ Gemfile
@@ -23,7 +23,7 @@ gem 'jbuilder', '~> 2.7'
 gem 'bcrypt', '~> 3.1.7'
 gem 'rspec', '~> 3.0'
 gem 'font-awesome-sass', '~> 5.13.0'
-
+gem 'rspec-rails'
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
 
---
+++ spec/models/client_model_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+require 'rspec/rails'
+
+
+RSpec.describe Client do
+    before do
+        @vz = Isp.create(name: 'Verizon FiOS', email: 'support@verizon.com')
+        @pn = Client.create(name: 'Paul Nicholsen', username: "p.nicholsen", password: "password", phone_number: '202 555 5678', email: 'p.nicholsen@flatiron.com', home_address: '1600 Pennsylvania Ave', isp_id: @vz.id)
+    end
+    describe "#first_name" do
+        it "returns the first name of the given person" do
+            expect(@pn.first_name).to eq("Paul")
+        end
+    end
+
+    describe "#last_name" do
+        it "returns the last name of the given person" do
+            expect(@pn.last_name).to eq("Nicholsen")
+        end
+    end
+
+    describe "#ISP" do
+        it "returns the name of the given person's ISP, testing the relation" do
+            expect(@pn.isp.name).to eq("Verizon FiOS")
+        end
+    end
+
+    describe "#"
+end
